3ba0ed44acf95c82a5cfab660a86d1470291d126
galy <galy@student.42.fr>
2020-04-10 17:16:14 +0200

    pb de camera ok

--- view/pages/studio.php
+++ view/pages/studio.php
@@ -18,214 +18,152 @@
 	</div>
 	<div class="galerieBox">
 		<?php
-			if(isset($userPics))
-			{
-				$i = count($userPics) - 1;
-				while($i >= 0)
-				{
-					echo "<div class=\"imgWrap\" >";
-					echo "<img src=\"".$userPics[$i]."\" />";
-					echo "<form method=\"POST\" action=\"studioDelPic\">";
-					echo "<input name=\"pic\" value=\"".$userPics[$i]."\"/>";
-					echo "<button>Delete</button>";
-					echo "</form>";
-					echo "</div>";
-					$i--;
-				}
+		if (isset($userPics)) {
+			$i = count($userPics) - 1;
+			while ($i >= 0) {
+				echo "<div class=\"imgWrap\" >";
+				echo "<img src=\"" . $userPics[$i] . "\" />";
+				echo "<form method=\"POST\" action=\"studioDelPic\">";
+				echo "<input name=\"pic\" value=\"" . $userPics[$i] . "\"/>";
+				echo "<button>Delete</button>";
+				echo "</form>";
+				echo "</div>";
+				$i--;
 			}
+		}
 		?>
 	</div>
 
 	<div class="calquesBox">
-			<?php
-			if(isset($calcsUrl))
-			{
-				for($i=0; $i < count($calcsUrl); $i++)
-				{
-					echo "<img class=\"calcImg\" src=\"".$calcsUrl[$i]."\" onclick=\"calcSelector(this)\"/>";
-				}
+		<?php
+		if (isset($calcsUrl)) {
+			for ($i = 0; $i < count($calcsUrl); $i++) {
+				echo "<img class=\"calcImg\" src=\"" . $calcsUrl[$i] . "\" onclick=\"calcSelector(this)\"/>";
 			}
-			?>
+		}
+		?>
 	</div>
 
 </div>
 <form class="hiddenForm" method="POST" action="/pages/picRegistration">
-	<input id="dataSendPic" type="image/png" name="picData" value="none"/>
-	<input id="dataSendCalc" type="image/png" name="calcData" value="none"/>
-	<input id="uplInp" type="file" name="uplData" accept="image/png, image/jpeg, image/jpg" onchange="showFile(this.files)"/>
+	<input id="dataSendPic" type="image/png" name="picData" value="none" />
+	<input id="dataSendCalc" type="image/png" name="calcData" value="none" />
+	<input id="uplInp" type="file" name="uplData" accept="image/png, image/jpeg, image/jpg" onchange="showFile(this.files)" />
 </form>
 
+
+
 <script type="text/javascript">
+	startVideoStream();
+	async function startVideoStream() {
+		const media = {
+			width: 1024,
+			height: 768,
+			stream: undefined
+		};
 
-(function()
-{
-	var streaming = false,
-	video		= document.querySelector("#video"),
-	photo		= document.querySelector("#photo"),
-	picTakeBtn	= document.querySelector("#picTakeBtn"),
-	width 		= 1024,
-	height 		= 0;
+		media.stream = await navigator.mediaDevices.getUserMedia({
+			video: true,
+			width: media.width,
+			heigth: media.height,
+		});
+		const video = document.querySelector("#video");
+		const photo = document.querySelector("#photo");
 
-	navigator.getMedia	= (navigator.getUserMedia ||
-		navigator.webkitGetUserMedia ||
-		navigator.mozGetUserMedia ||
-		navigator.msGetUserMedia);
-
-	navigator.getMedia(
-	{
-		video: true,
-		audio: false
-	},
-function(stream)
-{
-	console.log("stream:"+stream);
-	if (navigator.mozGetUserMedia)
-	{
-		video.mozSrcObject = stream;
-	}
-	else
-	{
-		var vendorURL = window.URL || window.webkitURL;
-		video.src = vendorURL.createObjectURL(stream);
-	}
-	video.play();
-},
-function(err)
-{
-	console.log("An error occured! " + err);
-	var video = document.getElementsByClassName("preview");
-	var videoBox = document.getElementsByClassName("videoBox");
-	videoBox[0].removeChild(video[0]);
-	photo = null;
-});
-
-video.addEventListener("canplay",
-function(ev)
-{
-	if (!streaming)
-	{
-		height = video.videoHeight / (video.videoWidth/width);
-		video.setAttribute("width", width);
-		video.setAttribute("height", height);
-		photo.setAttribute("width", width);
-		photo.setAttribute("height", height);
-		streaming = true;
-	}
-},false);
-
-picTakeBtn.addEventListener("click",
-function(ev)
-{
-	if(document.querySelector("#uplInp").value != "")
-	{
-		var uplobj = document.querySelector(".uplobj");
-		if(uplobj)
-		{
-			var uplVal = uplobj.getAttribute("src");
-			if(uplVal === null)
-			{
-				alert("uplVal="+uplVal+" Veuillez uploder une photo ou activer votre webcam");
+		// Older browsers may not have srcObject
+		if ('srcObject' in video) {
+			try {
+				video.srcObject = media.stream;
+			} catch (error) {
+				console.error(error);
+				// Even if they do, they may only support MediaStream
+				video.src = URL.createObjectURL(media.stream);
 			}
-			else
-			{
-				var picData = document.querySelector(".uplObj").getAttribute("src");
-				document.querySelector("#dataSendPic").setAttribute("value", picData);
-				document.querySelector(".hiddenForm").submit();
-			}
-		}
-	}
-	else if(photo != null)
-	{
-		photo.getContext("2d", {alpha: true}).drawImage(video, 0, 0, width, height);
-		var data = photo.toDataURL("image/png");
-		photo.setAttribute("src", data);
-		var picData = document.querySelector("#photo").getAttribute("src");
-		document.querySelector("#dataSendPic").setAttribute("value", picData);
-		document.querySelector(".hiddenForm").submit();
-		ev.preventDefault();
-	}
-},false);
-})();
-
-
-function calcSelector(me)
-{
-	var calcs = document.getElementsByClassName("calcImg");
-	var calcUrl = me.getAttribute("src");
-	//je set la visualisation de la selection du calc
-	for (i = 0; i < calcs.length; i++)
-	{
-		calcs[i].style.border = "none";
-		calcs[i].style.opacity = "0.3";
-	}
-	me.style.border = "1px dotted #ffffff";
-	me.style.opacity = "1";
-	var calcData = me.getAttribute("src");
-	document.querySelector("#dataSendCalc").setAttribute("value", calcData);
-
-	var btnTakePic = document.querySelector(".btnBox ul");
-	btnTakePic.style.display = "block";
-
-	var upperLayer = document.querySelector(".upperLayer");
-	upperLayer.innerHTML = "";
-	var upLayer_img = document.createElement("img");
-	var videoW = document.querySelector("#video").clientWidth;
-	var videoH = document.querySelector("#video").clientHeight;
-	var uplObj = document.querySelector(".uplObj");
-	if(videoW == 0 && videoH == 0)
-	{
-		upLayer_img.setAttribute("width", uplObj.clientWidth);
-		upLayer_img.setAttribute("height", uplObj.clientHeight);
-	}
-	else
-	{
-		upLayer_img.setAttribute("width", videoW);
-		upLayer_img.setAttribute("height", videoH);
-	}
-	upLayer_img.setAttribute("src", calcUrl);
-	upperLayer.appendChild(upLayer_img);
-}
-
-function showFile(files)
-{
-	for (var i = 0; i < files.length; i++)
-	{
-		var file = files[i];
-		var imageType = /^image\//;
-
-		if (!imageType.test(file.type))
-		{
-			continue;
-		}
-		var img = document.createElement("img");
-		img.classList.add("uplObj");
-		img.file = file;
-
-		var image = document.getElementsByClassName("uplObj");
-		if(image[0])
-		{
-			var videoBox = document.getElementsByClassName("videoBox");
-			videoBox[0].removeChild(image[0]);
+		} else {
+			video.src = URL.createObjectURL(media.stream);
 		}
 
-		var videoBox = document.getElementsByClassName("videoBox");
-		document.querySelector(".videoBox").appendChild(img);
+		video.setAttribute("width", media.width);
+		video.setAttribute("height", media.height);
+		photo.setAttribute("width", media.width);
+		photo.setAttribute("height", media.height);
+			
+		// then press play :)
+		video.play();
+	};
 
-		var upperLayer = document.querySelector(".upperLayer");
+
+	function calcSelector(me) {
 		var calcs = document.getElementsByClassName("calcImg");
-		upperLayer.innerHTML = "";
-		for (i = 0; i < calcs.length; i++)
-		{
+		var calcUrl = me.getAttribute("src");
+		//je set la visualisation de la selection du calc
+		for (i = 0; i < calcs.length; i++) {
 			calcs[i].style.border = "none";
 			calcs[i].style.opacity = "0.3";
 		}
+		me.style.border = "1px dotted #ffffff";
+		me.style.opacity = "1";
+		var calcData = me.getAttribute("src");
+		document.querySelector("#dataSendCalc").setAttribute("value", calcData);
 
-		var reader = new FileReader();
-		reader.onload = (function(aImg) { return function(e) { aImg.src = e.target.result; }; })(img);
-		reader.readAsDataURL(file);
-		var video = document.querySelector("#video");
-		video.style.display = "none";
+		var btnTakePic = document.querySelector(".btnBox ul");
+		btnTakePic.style.display = "block";
+
+		var upperLayer = document.querySelector(".upperLayer");
+		upperLayer.innerHTML = "";
+		var upLayer_img = document.createElement("img");
+		var videoW = document.querySelector("#video").clientWidth;
+		var videoH = document.querySelector("#video").clientHeight;
+		var uplObj = document.querySelector(".uplObj");
+		if (videoW == 0 && videoH == 0) {
+			upLayer_img.setAttribute("width", uplObj.clientWidth);
+			upLayer_img.setAttribute("height", uplObj.clientHeight);
+		} else {
+			upLayer_img.setAttribute("width", videoW);
+			upLayer_img.setAttribute("height", videoH);
+		}
+		upLayer_img.setAttribute("src", calcUrl);
+		upperLayer.appendChild(upLayer_img);
 	}
-}
 
+	function showFile(files) {
+		for (var i = 0; i < files.length; i++) {
+			var file = files[i];
+			var imageType = /^image\//;
+
+			if (!imageType.test(file.type)) {
+				continue;
+			}
+			var img = document.createElement("img");
+			img.classList.add("uplObj");
+			img.file = file;
+
+			var image = document.getElementsByClassName("uplObj");
+			if (image[0]) {
+				var videoBox = document.getElementsByClassName("videoBox");
+				videoBox[0].removeChild(image[0]);
+			}
+
+			var videoBox = document.getElementsByClassName("videoBox");
+			document.querySelector(".videoBox").appendChild(img);
+
+			var upperLayer = document.querySelector(".upperLayer");
+			var calcs = document.getElementsByClassName("calcImg");
+			upperLayer.innerHTML = "";
+			for (i = 0; i < calcs.length; i++) {
+				calcs[i].style.border = "none";
+				calcs[i].style.opacity = "0.3";
+			}
+
+			var reader = new FileReader();
+			reader.onload = (function(aImg) {
+				return function(e) {
+					aImg.src = e.target.result;
+				};
+			})(img);
+			reader.readAsDataURL(file);
+			var video = document.querySelector("#video");
+			video.style.display = "none";
+		}
+	}
 </script>
